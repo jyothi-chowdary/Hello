9d3d1724f4fbc181cf69ac60139cbac9a461afdf
jyothi-chowdary <jyothigutlapalli33@gmail.com>
2025-07-13 19:18:32 +0530

    Add files via upload

---
+++ Concadination.java
@@ -0,0 +1,10 @@
+class Concadination{
+        public static void main(String[] args){
+
+                System.out.println("NAME:" +" Jyothi");
+                System.out.println("AGE:" +"20");  
+                System.out.println("GENDER:" +" Female");
+                 
+         }
+}
+

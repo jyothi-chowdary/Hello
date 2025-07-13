2cf4b7b23bee8ba58da52395220add34df97d2a1
jyothi-chowdary <jyothigutlapalli33@gmail.com>
2025-07-13 19:00:07 +0530

    Add files via upload

---
+++ PersonDetails.java
@@ -0,0 +1,12 @@
+class PersonDetails{
+	public static void main(String[] args){
+		
+	System.out.print("NAME: ");
+	System.out.println(args[0]);
+	System.out.print("AGE: ");
+	System.out.println(args[1]);
+	System.out.print("GENDER: ");
+	System.out.println(args[2]);
+	}
+}
+

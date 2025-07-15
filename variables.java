7fb52352cd0138c5cb3bc0d15229632418e81758
jyothi-chowdary <jyothigutlapalli33@gmail.com>
2025-07-15 17:53:53 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,12 @@
+class variables{
+	public static void displayInfo(){
+ 	String studentname="Srilatha";
+	System.out.println(studentname);
+}
+public static void main(String[]args){
+displayInfo();
+int age=30;
+System.out.println(age);
+System.out.println("age:"+age);
+}
+}
